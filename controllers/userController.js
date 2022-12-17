dffe605bf99fa923541267ca6accc2c84a263107
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-12-17 15:49:56 -0600

    Start building routes and controllers

---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+const { ObjectId } = require('mongoose').Types // ??
+const { User, Thought } = require('../models')
+
+module.exports = {
+    getUsers(){},
+
+    createUser(){}
+}
+
+// Reference: 28-Stu_Mini-Project > contollers > studentController.js
